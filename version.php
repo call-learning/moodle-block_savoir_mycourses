deb648db15e58796a0b448e81a77c00d3ee0b084
Laurent David <lmedavid@gmail.com>
2019-08-28 10:47:39 +0200

    Remove course description in list

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018051706;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2018051708;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component = 'block_savoir_mycourses'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [
